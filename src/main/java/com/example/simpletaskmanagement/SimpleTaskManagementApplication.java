e10ba3d8258d2b2d22aec47c1df9dedcdedbf514
dan <dochoa6658@gmail.com>
2024-10-24 16:00:44 -0500

    Initial Commit

---
+++ src/main/java/com/example/simpletaskmanagement/SimpleTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpletaskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTaskManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleTaskManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simpletaskmanagement/SimpleTaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpletaskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTaskManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
